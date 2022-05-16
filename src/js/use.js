9a195c3ea053a8cea41004707d6da1024f286613
r2dev2 <ronak.badhe@gmail.com>
2022-05-16 10:13:30 -0700

    fix: use contenteditable shenanigans to enable entry for android

--- src/js/use.js
+++ src/js/use.js
@@ -37,7 +37,7 @@ export const replaceableElement = (node, options_ = {}) => {
   /** @type {(e: KeyboardEvent) => void} */
   const onKeyDown = (e) => {
     if (options.disabled) return;
-    if (e.key !== 'Tab') e.preventDefault();
+    node.setAttribute('contenteditable', 'false');
 
     if (options.ogchar === e.key.toUpperCase()) {
       dispatch('error', {
@@ -53,7 +53,12 @@ export const replaceableElement = (node, options_ = {}) => {
     });
   };
 
+  const onFocus = () => {
+    node.setAttribute('contenteditable', 'true');
+  };
+
   node.addEventListener('keydown', onKeyDown);
+  node.addEventListener('focus', onFocus);
 
   return {
     update(newOptions) {
@@ -63,6 +68,7 @@ export const replaceableElement = (node, options_ = {}) => {
 
     destroy() {
       node.removeEventListener('keydown', onKeyDown);
+      node.removeEventListener('focus', onFocus);
     },
   };
 };
